bd11fed157e99bd836299183cbfd82ef618d0d8c
Andrey Yurovsky <yurovsky@gmail.com>
2012-08-14 22:15:48 -0700

    improve init

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stm32f10x.h>
 
-/* Scheduler includes. */
+#include <uart.h>
+
 #include <FreeRTOS.h>
 #include <task.h>
 #include <queue.h>
@@ -18,20 +19,15 @@ void vApplicationTickHook(void)
 {
 }
 
-static void clock_init(void)
-{
-}
-
 static void timer_init(void)
 {
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 }
 
 int main(void)
 {
-	clock_init();
 	timer_init();
+	uart_init(115200);
 
 	vTaskStartScheduler();
 
